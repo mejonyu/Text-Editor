f726c405f69bc0e29fbe8be3e53072832820acf5
Jonathan Yu <jonyu02@gmail.com>
2021-07-13 18:18:26 -0400

    add period to destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-        flash[:notice] = "You have been signed out!"
+        flash[:notice] = "You have been signed out."
         redirect_to new_session_path
     end
 
