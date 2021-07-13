9e01d7ff634196fb1fe0c44841c3aeee0be8d3f6
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-07-13 18:46:23 -0400

    nav

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show]
   resources :essay_contributors
   resources :sessions, only: [:new, :create, :destroy]
+
+  get '/signup', to: 'users#new'
+  post '/signup-attempt', to: 'users#create'
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
 end
