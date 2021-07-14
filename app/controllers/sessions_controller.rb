8623968e1ff82f498bd2da7d003e77b7d599da7f
Jonathan Yu <jonyu02@gmail.com>
2021-07-13 21:39:28 -0400

    update nav bar labels, fix signin_path bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
             redirect_to @user
         else
             flash[:notice] = "Login is invalid."
-            redirect_to new_session_path
+            redirect_to signin_path
         end
     end
 
